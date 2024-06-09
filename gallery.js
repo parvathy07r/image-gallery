2421e441b4c640d01420a56d22c88b89453c1a23
Parvathy R <parvathy07r@gmail.com>
2024-06-09 16:43:55 +0530

    Added animation

--- gallery.js
+++ gallery.js
@@ -26,3 +26,27 @@ nextBtn.addEventListener('click', function () {
     }
 
 });
+
+const { styler, spring, listen, pointer, value } = window.popmotion;
+
+const ball = document.querySelector('h1');
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(ball, 'mousedown touchstart')
+    .start((e) => {
+        e.preventDefault();
+        pointer(ballXY.get()).start(ballXY);
+    });
+
+listen(document, 'mouseup touchend')
+    .start(() => {
+        spring({
+            from: ballXY.get(),
+            velocity: ballXY.getVelocity(),
+            to: { x: 0, y: 0 },
+            stiffness: 200,
+            // mass: 1,
+            // damping: 10
+        }).start(ballXY);
+    });
